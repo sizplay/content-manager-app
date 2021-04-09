a6115ee04a003d679d18e9e1b64865c0cceb40cc
Chaehoon Lim <sizplay@gmail.com>
2021-04-10 08:34:31 +0900

    finish class component lecture

---
+++ pages/_.app.js
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+
+const MainApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MainApp;
---
+++ pages/about.js
@@ -0,0 +1,5 @@
+const about = () => {
+  return <h1>I am about page</h1>;
+};
+
+export default about;
